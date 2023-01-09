34d3ed609ff056ad3b7d15b3d2fc07e0d3c240fc
Warren <nexources@nexources.com>
2023-01-09 16:13:54 +0300

    [INIT] stimco_sale_stock

---
+++ stimco_sale_stock/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Stimco Sales and Warehouse Management',
+    'version': '1.0',
+    'category': 'Hidden',
+    'depends': ['sale_stock'],
+    'data': [
+        'views/sale_order_views.xml'
+    ],
+    'license': 'LGPL-3',
+}
