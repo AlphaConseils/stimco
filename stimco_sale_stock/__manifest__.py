220161d8e1e718e873e3b8ec60434009537868ae
Warren <nexources@nexources.com>
2023-01-11 12:05:35 +0300

    [FEAT] stimco_sale_stock: rename manifest

--- stimco_sale_stock/__manifest__.py
+++ stimco_sale_stock/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Stimco Sales and Warehouse Management',
+    'name': 'STIMCO Sales and Warehouse Management',
     'version': '1.0',
     'category': 'Hidden',
     'depends': ['sale_stock'],
